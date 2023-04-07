25cf2a54a2c35cafdc4a3440f1ebdc4a6a00caec
ggsoon <88361127+ggsoon@users.noreply.github.com>
2023-04-07 17:40:33 +0900

    Initial commit

---
+++ src/test/java/com/food/withkitchen/WithkitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.withkitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithkitchenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
