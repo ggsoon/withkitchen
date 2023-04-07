c89659e45d4f971e52c6af670b6df04a16a8de74
ggsoon <88361127+ggsoon@users.noreply.github.com>
2023-04-07 17:40:33 +0900

    Initial commit

---
+++ src/main/java/com/food/withkitchen/WithkitchenApplication.java
@@ -0,0 +1,13 @@
+package com.food.withkitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithkitchenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithkitchenApplication.class, args);
+    }
+
+}
