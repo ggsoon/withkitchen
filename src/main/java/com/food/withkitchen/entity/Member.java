d03abe3eea6d3914617c3482d13891ecda9e4a6e
ggsoon <88361127+ggsoon@users.noreply.github.com>
2023-04-08 21:46:29 +0900

    feat: UserDetails를 구현

--- src/main/java/com/food/withkitchen/entity/Member.java
+++ src/main/java/com/food/withkitchen/entity/Member.java
@@ -1,8 +1,15 @@
 package com.food.withkitchen.entity;
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Entity
@@ -14,11 +21,18 @@ import javax.persistence.*;
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 @Table(name = "member")
-public class Member extends BaseEntity {
+public class Member extends BaseEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, updatable = true)
+    private String uid; // JWT 토큰 내 정보
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
 
     @Column(nullable = false, unique = true, length = 15)
     private String name;
@@ -28,4 +42,38 @@ public class Member extends BaseEntity {
 
     @Column(nullable = false, unique = true, length = 50)
     private String email;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return this.uid;
+    }
+
+    // 계정이 만료되었는지 확인하는 로직
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    // 계정이 잠겼는지 확인하는 로직, 사용하지 않으므로 true
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    // 계정의 패스워드가 만료되었는지 확인하는 로직
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    // 계정이 사용가능한지 확인하는 로직
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
