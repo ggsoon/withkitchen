4f698f8f66b0406c7cb65c1898ed36ca499c03fb
ggsoon <88361127+ggsoon@users.noreply.github.com>
2023-04-08 21:46:47 +0900

    feat: JpaRepository를 확장

--- src/main/java/com/food/withkitchen/repository/MemberRepository.java
+++ src/main/java/com/food/withkitchen/repository/MemberRepository.java
@@ -1,4 +1,10 @@
 package com.food.withkitchen.repository;
 
-public class MemberRepository {
+import com.food.withkitchen.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Member getByUid(String uid);
 }
