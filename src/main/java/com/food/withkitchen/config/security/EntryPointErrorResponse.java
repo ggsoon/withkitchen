e393e01c31ff3f5537e417d1e66e7310e3602653
ggsoon <88361127+ggsoon@users.noreply.github.com>
2023-04-08 22:33:12 +0900

    feat: 인증되지 않는 요청에 응답하는 오류 메시지 포함함

---
+++ src/main/java/com/food/withkitchen/config/security/EntryPointErrorResponse.java
@@ -0,0 +1,15 @@
+package com.food.withkitchen.config.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class EntryPointErrorResponse {
+
+    private String msg;
+}
