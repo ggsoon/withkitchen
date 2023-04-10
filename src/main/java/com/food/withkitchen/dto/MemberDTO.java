73a686ca6f8b1ee70cc334ffca1216067b784d99
ggsoon <88361127+ggsoon@users.noreply.github.com>
2023-04-10 11:20:11 +0900

    feat: DTO 생성

--- src/main/java/com/food/withkitchen/dto/MemberDTO.java
+++ src/main/java/com/food/withkitchen/dto/MemberDTO.java
@@ -1,4 +1,17 @@
 package com.food.withkitchen.dto;
 
+import lombok.*;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
 public class MemberDTO {
+
+    private Long id;
+
+    private String username;
+
 }
