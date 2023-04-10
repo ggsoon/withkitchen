a508488bd1c7cdd49d777069642614a20959dd38
ggsoon <88361127+ggsoon@users.noreply.github.com>
2023-04-10 11:21:13 +0900

    feat: Menu를 제어하는 Controller 생성

---
+++ src/main/java/com/food/withkitchen/controller/MenuController.java
@@ -0,0 +1,37 @@
+package com.food.withkitchen.controller;
+
+import com.food.withkitchen.entity.Member;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MenuController {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/home";
+    }
+
+    @GetMapping("/home")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/shop")
+    public String shop() {
+        return "/shop";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "/about";
+    }
+
+    @GetMapping("/signin")
+    public String signin() {
+        return "signin";
+    }
+
+
+}
