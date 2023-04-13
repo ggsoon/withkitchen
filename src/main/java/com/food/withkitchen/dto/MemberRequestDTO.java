e959d7d9b591c1d3b916622e993640b8ec4531f3
ggsoon <88361127+ggsoon@users.noreply.github.com>
2023-04-14 02:01:37 +0900

    feat: role 필드 추가

--- src/main/java/com/food/withkitchen/dto/MemberRequestDTO.java
+++ src/main/java/com/food/withkitchen/dto/MemberRequestDTO.java
@@ -24,11 +24,14 @@ public class MemberRequestDTO {
     @Pattern(regexp = "^(?=.*\\d)(?=.*[a-zA-Z])[a-zA-Z\\d]{8,16}$", message = "비밀번호는 8~16자리로 숫자 혹은 영문 대소문자 1개 이상 포함해야 하며 특수문자는 사용할 수 없습니다.")
     private String password;
 
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "유효하지 않은 이메일 주소입니다.")
+    private String email;
+
     @NotBlank(message = "닉네임을 입력해주세요.")
     @Pattern(regexp = "^[가-힣a-zA-Z0-9]{2,10}$", message = "닉네임은 2~10자리로 특수문자는 사용할 수 없습니다.")
     private String nickname;
 
-    @NotBlank(message = "이메일을 입력해주세요.")
-    @Email(message = "유효하지 않은 이메일 주소입니다.")
-    private String email;
+    @Pattern(regexp = "^(|ADMIN)$", message = "관리자 코드를 모르실경우 아무것도 입력하지 말아 주세요.")
+    private String role;
 }
