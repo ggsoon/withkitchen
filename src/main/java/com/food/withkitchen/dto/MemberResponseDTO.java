236024df5b3a60af2822b4aea0181d2174de6c31
ggsoon <88361127+ggsoon@users.noreply.github.com>
2023-04-14 17:54:51 +0900

    refactor: 회원가입 응답 코드로 변경

--- src/main/java/com/food/withkitchen/dto/MemberResponseDTO.java
+++ src/main/java/com/food/withkitchen/dto/MemberResponseDTO.java
@@ -4,19 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.validation.BindingResult;
 
-// API 응답 결과를 나타내어 필요한 정보만 담아 응답 결과를 표현한다, API 응답 결과가 변경되어도 비즈니스 로직 처리와 관련이 없어 영향을 최소화
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 public class MemberResponseDTO {
 
-    private Long id;
+    private int code;
 
-    private String username;
+    private String msg;
 
-    private String password;
-
-    private String email;
 }
