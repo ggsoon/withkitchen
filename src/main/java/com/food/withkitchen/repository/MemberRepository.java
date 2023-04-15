c34a4b4bf0d546bf09ec4cdbeced07951f88634f
ggsoon <88361127+ggsoon@users.noreply.github.com>
2023-04-15 19:31:47 +0900

    feat: 중복 검사 추가

--- src/main/java/com/food/withkitchen/repository/MemberRepository.java
+++ src/main/java/com/food/withkitchen/repository/MemberRepository.java
@@ -2,9 +2,11 @@ package com.food.withkitchen.repository;
 
 import com.food.withkitchen.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
-    Member getByUid(String uid);
+    Optional<Member> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
 }
